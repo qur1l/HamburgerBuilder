bcf5580c12af7d9c722cb2b8980e1bdcca16da8d
qur1l <144834356+qur1l@users.noreply.github.com>
2024-05-20 10:39:15 +0300

    Create tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,27 @@
+TEST(HamburgerBuilderTest, InitialState) {
+    HamburgerBuilder vm;
+    EXPECT_EQ(vm.GetCpuCount(), 1);
+    EXPECT_EQ(vm.GetRamSize(), 500);
+}
+
+TEST(HamburgerBuilderTest, ChangeCpuCount) {
+    HamburgerBuilder vm;
+    vm.ChangeCpuCount(4);
+    EXPECT_EQ(vm.GetCpuCount(), 4);
+}
+
+TEST(HamburgerBuilderTest, ChangeRamSize) {
+    HamburgerBuilder vm;
+    vm.ChangeRamSize(2000);
+    EXPECT_EQ(vm.GetRamSize(), 2000);
+}
+
+TEST(HamburgerBuilderTest, ResetToSnapshot) {
+    HamburgerBuilder vm;
+    auto snapshot = vm.TakeSnapshot();
+    vm.ChangeCpuCount(4);
+    vm.ChangeRamSize(2000);
+    vm.ResetToSnapshot(snapshot);
+    EXPECT_EQ(vm.GetCpuCount(), 1);
+    EXPECT_EQ(vm.GetRamSize(), 500);
+}
