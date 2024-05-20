759b9de33c145a377458d561d8e878445f3186e7
qur1l <144834356+qur1l@users.noreply.github.com>
2024-05-20 10:59:21 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -20,9 +20,36 @@ TEST(HamburgerTest, HamburgerPrintTest) {
     EXPECT_EQ(output, "----------\nBread\nPatty\nCheese\nTomato\nLettuce\nSauce\n----------\n");
 }
 
-// Add more test cases as needed
+// Test case for Hamburger class with no ingredients
+TEST(HamburgerTest, HamburgerNoIngredientsTest) {
+    std::vector<std::unique_ptr<HamburgerIngredient>> ingredients;
 
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    Hamburger hamburger(std::move(ingredients));
+
+    testing::internal::CaptureStdout();
+    hamburger.Print();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "----------\n----------\n");
 }
+
+// Test case for HamburgerBuilder class
+TEST(HamburgerBuilderTest, HamburgerBuilderTest) {
+    BeefHamburgerBuilder builder;
+    builder.Initialize();
+    builder.BuildBread();
+    builder.BuildPatty();
+    builder.BuildCheese();
+    builder.BuildTomato();
+    builder.BuildLettuce();
+    builder.BuildSauce();
+
+    Hamburger hamburger = builder.GetHamburger();
+
+    testing::internal::CaptureStdout();
+    hamburger.Print();
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "----------\nLightBread\nBeefPatty\nMozzarellaCheese\nTomato\nLettuce\nGarlicSauce\n----------\n");
+}
+
